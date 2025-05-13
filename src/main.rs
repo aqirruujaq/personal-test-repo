8ff2a2b32c9dce2535c5f1f85ddc19c3bb4131ba
aqirruujaq <aqirruujaq@outlook.com>
2025-05-13 11:13:48 +0800

    update

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use dioxus::prelude::*;
+
+fn main() {
+    launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Stylesheet { href: asset!("assets/main.css") }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
+#[component]
+fn DogView() -> Element {
+    let skip = move |_| {};
+    let save = move |_| {};
+    rsx! {
+        div { id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div { id: "buttons",
+            button { onclick: skip, id: "skip", "skip" }
+            button { onclick: save, id: "save", "save!" }
+        }
+    }
+}
